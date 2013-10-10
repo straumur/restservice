f44d6968c5151b7b2ebb82b228cac317488b1790
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-10-10 23:15:01 +0000

    rest dataservice, initial commit

---
+++ handlers.go
@@ -0,0 +1,231 @@
+package rest
+
+//TODO: Better use of errchan
+import (
+	"encoding/json"
+	"errors"
+	"github.com/golang/glog"
+	"github.com/gorilla/mux"
+	"github.com/straumur/straumur"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	ErrSaveExisting      = errors.New("Save existing resource")
+	ErrUpdateNonExisting = errors.New("Update non-existing resource")
+	ErrMissingType       = errors.New("Missing type param")
+	ErrInvalidEntity     = errors.New("Invalid entity")
+)
+
+type RESTService struct {
+	databackend straumur.DataBackend
+	address     string
+	prefix      string
+	events      chan *straumur.Event
+}
+
+// Returns the entity prefix
+func getEntity(req *http.Request) (string, error) {
+	vars := mux.Vars(req)
+	entity := vars["entity"]
+	id := vars["id"]
+	if id == "" || entity == "" {
+		return "", ErrInvalidEntity
+	}
+	return entity + "/" + id, nil
+}
+
+// Parses a Query from the request
+func getQuery(req *http.Request) (*straumur.Query, error) {
+	req.ParseForm()
+	return straumur.QueryFromValues(req.Form)
+}
+
+// Parses an event from the post body
+func (r *RESTService) parseEvent(body io.ReadCloser) (straumur.Event, error) {
+	decoder := json.NewDecoder(body)
+	defer body.Close()
+	var e straumur.Event
+	err := decoder.Decode(&e)
+	return e, err
+}
+
+// Wraps http.HandlerFunc, adds error response and frequently used headers
+func handlerWrapper(f func(http.ResponseWriter, *http.Request) (error, int)) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+		t := time.Now()
+		err, status := f(w, r)
+
+		glog.V(2).Infof("Processed request: %s:%s in %f", r.Method, r.URL, time.Now().Sub(t).Seconds())
+
+		if err != nil {
+			glog.Warningf("Error - [%s]%s, status: %d", r.Method, r.URL, status)
+			http.Error(w, err.Error(), status)
+		}
+	}
+}
+
+// GET: /api/entity/id/
+func (r *RESTService) entityHandler(w http.ResponseWriter, req *http.Request) (error, int) {
+	e, err := getEntity(req)
+	if err != nil {
+		return err, http.StatusBadRequest
+	}
+	q, err := getQuery(req)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	q.Entities = append(q.Entities, e)
+	events, err := r.databackend.Query(*q)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	enc := json.NewEncoder(w)
+	enc.Encode(events)
+	return nil, http.StatusOK
+}
+
+// GET: /api/id/
+func (r *RESTService) retrieveHandler(w http.ResponseWriter, req *http.Request) (error, int) {
+
+	vars := mux.Vars(req)
+	id := vars["id"]
+	idAsInt, err := strconv.Atoi(id)
+	if err != nil {
+		return err, http.StatusBadRequest
+	}
+	event, err := r.databackend.GetById(idAsInt)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	enc := json.NewEncoder(w)
+	enc.Encode(event)
+	return nil, http.StatusOK
+}
+
+// POST: /
+// PUT: /id/
+// Save or update
+func (r *RESTService) saveHandler(w http.ResponseWriter, req *http.Request) (error, int) {
+
+	vars := mux.Vars(req)
+	id := vars["id"]
+	e, err := r.parseEvent(req.Body)
+	if err != nil {
+		return err, http.StatusBadRequest
+	}
+	if id == "" && e.ID != 0 {
+		return ErrSaveExisting, http.StatusBadRequest
+	}
+	if id != "" && e.ID == 0 {
+		return ErrUpdateNonExisting, http.StatusBadRequest
+	}
+	if e.ID == 0 {
+		w.WriteHeader(http.StatusCreated)
+	} else {
+		w.WriteHeader(http.StatusAccepted)
+	}
+	r.events <- &e
+	return nil, 0
+}
+
+// GET: /api/search
+func (r *RESTService) searchHandler(w http.ResponseWriter, req *http.Request) (error, int) {
+	q, err := getQuery(req)
+	if err != nil {
+		return err, http.StatusBadRequest
+	}
+	events, err := r.databackend.Query(*q)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+	enc := json.NewEncoder(w)
+	enc.Encode(events)
+	return nil, http.StatusOK
+}
+
+func (r *RESTService) aggregateHandler(w http.ResponseWriter, req *http.Request) (error, int) {
+	vars := mux.Vars(req)
+	agtype := vars["type"]
+	if agtype == "" {
+		return ErrMissingType, http.StatusBadRequest
+	}
+	q, err := getQuery(req)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+
+	m, err := r.databackend.AggregateType(*q, agtype)
+	if err != nil {
+		return err, http.StatusInternalServerError
+	}
+
+	enc := json.NewEncoder(w)
+	enc.Encode(m)
+	return nil, http.StatusOK
+}
+
+func (r *RESTService) getRouter() (*mux.Router, error) {
+
+	router := mux.NewRouter()
+	s := router.PathPrefix(r.prefix).Subrouter()
+	s.HandleFunc("/{entity}/{id}/", handlerWrapper(r.entityHandler)).Methods("GET")
+	s.HandleFunc("/", handlerWrapper(r.saveHandler)).Methods("POST")
+	s.HandleFunc("/{id}/", handlerWrapper(r.retrieveHandler)).Methods("GET")
+	s.HandleFunc("/{id}/", handlerWrapper(r.saveHandler)).Methods("PUT")
+	s.HandleFunc("/search", handlerWrapper(r.searchHandler)).Methods("GET")
+	s.HandleFunc("/aggregate/{type}", handlerWrapper(r.aggregateHandler)).Methods("GET")
+	return router, nil
+}
+
+// Creates a new REST dataservice
+func NewRESTService(prefix string, address string) *RESTService {
+	return &RESTService{
+		prefix:  strings.TrimRight(prefix, "/"),
+		address: address,
+		events:  make(chan *straumur.Event),
+	}
+}
+
+//DataService interface
+func (r *RESTService) Run(d straumur.DataBackend, ec chan error) {
+
+	r.databackend = d
+
+	router, err := r.getRouter()
+	if err != nil {
+		ec <- err
+	}
+
+	http.Handle(r.prefix+"/", router)
+
+	err = http.ListenAndServe(r.address, nil)
+
+	glog.V(2).Info("Server started")
+
+	if err != nil {
+		ec <- err
+	}
+
+}
+
+//EventFeed interface
+func (r *RESTService) Updates() <-chan *straumur.Event {
+	return r.events
+}
+
+func (r *RESTService) Close() error {
+	close(r.events)
+	//Investigate way to perform graceful shutdown of http
+	return nil
+}
---
+++ handlers_test.go
@@ -0,0 +1,287 @@
+package rest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/google/go-querystring/query"
+	"github.com/straumur/straumur"
+	"log"
+	"net/http"
+	"sync"
+	"testing"
+)
+
+var serverAddr string
+var once sync.Once
+var client *http.Client
+var firstEvent *straumur.Event
+var secondEvent *straumur.Event
+var errChan chan error
+
+func startServer() {
+
+	errChan = make(chan error)
+	d := straumur.NewLocalMemoryStore()
+
+	firstEvent = straumur.NewEvent(
+		"myapp.user.login",
+		nil,
+		nil,
+		"User foobar logged in",
+		3,
+		"myapp",
+		[]string{"user/foo", "ns/moo"},
+		nil,
+		[]string{"actor1", "actor2"},
+		nil)
+
+	secondEvent = straumur.NewEvent(
+		"myapp.user.logout",
+		nil,
+		nil,
+		"User foobar logged out",
+		2,
+		"myapp",
+		[]string{"user/foo", "ns/moo"},
+		nil,
+		[]string{"actor1", "actor2", "actor3"},
+		nil)
+
+	err := d.Save(firstEvent)
+	if err != nil {
+		panic(err)
+	}
+
+	err = d.Save(secondEvent)
+	if err != nil {
+		panic(err)
+	}
+
+	serverAddr = "localhost:14234/api"
+	rest := NewRESTService("/api", ":14234")
+	go rest.Run(d, errChan)
+
+	go func() {
+		for {
+			select {
+			case err := <-errChan:
+				log.Fatalf("errChan: %+v", err)
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case event := <-rest.Updates():
+				d.Save(event)
+			}
+		}
+	}()
+
+	log.Print("Test Server running on ", serverAddr)
+	client = http.DefaultClient
+	log.Print("Test Client created")
+}
+
+func makeRequest(t *testing.T, method, url string, v interface{}) *http.Response {
+
+	var r *http.Response
+
+	t.Logf("[%s]: %s", method, url)
+
+	switch method {
+	case "POST", "PUT":
+		buf, err := json.Marshal(v)
+		if err != nil {
+			t.Errorf("Unable to serialize %v to json", v)
+		}
+
+		log.Printf("[%s] JSON: %s", method, string(buf))
+		req, err := http.NewRequest(method, url, bytes.NewReader(buf))
+		req.Header.Add("Content-Type", "application/json")
+		if err != nil {
+			t.Errorf("[%s] %s, error: %v", method, url, err)
+		}
+		r, err = client.Do(req)
+		if err != nil {
+			t.Errorf("Error when posting to %s, error: %v", url, err)
+		}
+	default:
+		r, err := client.Get(url)
+		if err != nil {
+			t.Errorf("Error: %v\n", err)
+		}
+
+		if r.StatusCode != http.StatusOK {
+			t.Errorf("Wrong status code: %d\n", r.StatusCode)
+		}
+
+		dec := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		err = dec.Decode(v)
+		if err != nil {
+			t.Errorf("Error: %v\n", err)
+		}
+	}
+
+	return r
+
+}
+
+func getJSON(t *testing.T, url string, v interface{}) *http.Response {
+	return makeRequest(t, "GET", url, v)
+}
+
+func postJSON(t *testing.T, url string, v interface{}) *http.Response {
+	return makeRequest(t, "POST", url, v)
+}
+
+func putJSON(t *testing.T, url string, v interface{}) *http.Response {
+	return makeRequest(t, "PUT", url, v)
+}
+
+func TestGetByEntity(t *testing.T) {
+	log.Println("TestGetByEntity")
+	once.Do(startServer)
+
+	url := fmt.Sprintf("http://%s/user/foo/", serverAddr)
+	t.Log(url)
+	events := []straumur.Event{}
+	getJSON(t, url, &events)
+	log.Printf("%v", events)
+}
+
+func TestGetById(t *testing.T) {
+	log.Println("TestGetById")
+	once.Do(startServer)
+
+	url := fmt.Sprintf("http://%s/1/", serverAddr)
+	t.Log(url)
+	event := straumur.Event{}
+	getJSON(t, url, &event)
+	log.Printf("%v", event)
+}
+
+func TestPostNewEvent(t *testing.T) {
+	log.Println("TestPostNewEvent")
+	once.Do(startServer)
+
+	e := straumur.Event{
+		Key:         "myapp.user.delete",
+		Description: "User foobar deleted",
+		Importance:  3,
+		Origin:      "myapp",
+		Entities:    []string{"user/foo"},
+	}
+
+	url := fmt.Sprintf("http://%s/", serverAddr)
+	r := postJSON(t, url, &e)
+	if r.StatusCode != http.StatusCreated {
+		t.Errorf("Status code expected %d, got %d", http.StatusCreated, r.StatusCode)
+	}
+	log.Print("Post succeded")
+}
+
+func TestPutEvent(t *testing.T) {
+	log.Println("TestPutEvent")
+	once.Do(startServer)
+
+	firstEvent.Description = "baz bar foo"
+	url := fmt.Sprintf("http://%s/1/", serverAddr)
+	r := putJSON(t, url, &firstEvent)
+	if r.StatusCode != http.StatusAccepted {
+		t.Errorf("Status code expected %d, got %d", http.StatusCreated, r.StatusCode)
+	}
+	log.Print("PUT succeded")
+}
+
+func TestSearch(t *testing.T) {
+	log.Println("TestSearch")
+	once.Do(startServer)
+
+	tests := []struct {
+		Q      straumur.Query
+		Status int
+	}{{
+		Q: straumur.Query{
+			Key: "myapp.user.login",
+		},
+		Status: http.StatusOK,
+	}, {
+		Q: straumur.Query{
+			Key: "myapp.user.login OR myapp.user.logout",
+		},
+		Status: http.StatusOK,
+	}}
+
+	for _, test := range tests {
+		v, err := query.Values(test.Q)
+		if err != nil {
+			t.Fatal(err)
+		}
+		url := fmt.Sprintf("http://%s/search?%s", serverAddr, v.Encode())
+		log.Println(url)
+		results := []straumur.Event{}
+		getJSON(t, url, &results)
+	}
+
+	q := straumur.Query{
+		Key: "myapp.user.login OR myapp.user.logout",
+	}
+	v, err := query.Values(q)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	url := fmt.Sprintf("http://%s/user/foo/?%s", serverAddr, v.Encode())
+	results := []straumur.Event{}
+	getJSON(t, url, &results)
+
+}
+
+func TestAggregateType(t *testing.T) {
+	once.Do(startServer)
+	q := straumur.Query{
+		Key: "myapp.user.login OR myapp.user.logout",
+	}
+	v, err := query.Values(q)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	url := fmt.Sprintf("http://%s/aggregate/actors?%s", serverAddr, v.Encode())
+	m := make(map[string]int)
+	getJSON(t, url, &m)
+
+	if m["actor1"] != 2 {
+		t.Fatalf("Expected 2, got %d, m: %+v", m["actor1"], m)
+	}
+
+	if m["actor3"] != 1 {
+		t.Fatalf("Expected 2, got %d, m: %+v", m["actor3"], m)
+	}
+
+}
+
+func TestImportanceFilter(t *testing.T) {
+
+	//Tests entity + filter
+	q := straumur.Query{
+		Importance: "lt3",
+	}
+	v, err := query.Values(q)
+	if err != nil {
+		t.Fatal(err)
+	}
+	url := fmt.Sprintf("http://%s/user/foo/?%s&from=01.01.1994", serverAddr, v.Encode())
+	log.Println(url)
+	results := []straumur.Event{}
+	getJSON(t, url, &results)
+
+	if len(results) != 1 {
+		t.Fatalf("Got %d, expected 1, q: %+v, events: %+v", len(results), q, results)
+	}
+
+}
